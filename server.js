c14283e7681bcbafd538f07cbf4df318b31538e5
Sidharth Mishra <visheshofc@gmail.com>
2026-06-17 18:30:03 +0530

    force ipv4 smtp

--- server.js
+++ server.js
@@ -421,14 +421,14 @@ const token = crypto.randomBytes(32).toString("hex");
     await newUser.save();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+  family: 4
 });
 
 console.log("Before SMTP Verify");
