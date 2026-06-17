1bd62d70cf2daedf97ec24e7bcefc539353c7f1d
Sidharth Mishra <visheshofc@gmail.com>
2026-06-17 17:21:20 +0530

    fixed smtp

--- server.js
+++ server.js
@@ -421,7 +421,9 @@ const token = crypto.randomBytes(32).toString("hex");
     await newUser.save();
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
